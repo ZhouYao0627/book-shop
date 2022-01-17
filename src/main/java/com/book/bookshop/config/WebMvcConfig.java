8c0b711aeb15a5cda06e6d005d52ab73d93be2ce
ZhouYao <1607435943@qq.com>
2022-01-17 15:45:56 +0800

    Initial Commit

---
+++ src/main/java/com/book/bookshop/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.book.bookshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ @author：ZhouYao
+ @create：2022-01-17 15:24
+ */
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/public/**").addResourceLocations
+                ("file:///D:/Pictures/images/");
+    }
+}
